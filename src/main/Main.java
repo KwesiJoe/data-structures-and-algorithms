2e52bccd2956666600da5e503bde797404d7a98c
major <majorkiema.mk@gmail.com>
2023-06-02 00:38:13 +0300

    commented the main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,11 @@
 /**
  * Main file of Linked List implentation in Java
  */
-package main;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("I'm doing DSA");
+package main;// This line declares the package name as "main"
+
+public class Main {// This line defines a public class named "Main"
+    public static void main(String[] args) {// This line defines the main method which serves as the entry point of the program
+        System.out.println("I'm doing DSA");//Prints the message "I'm doing DSA" to the console
     }
 }
